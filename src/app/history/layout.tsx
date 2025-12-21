60a1f51e357119cdcb16e1aa6b5575e363f2395b
Weda Wesnawa <komangweda360@gmail.com>
2025-12-21 22:17:02 +0800

    perbaiki eslint

--- src/app/history/layout.tsx
+++ src/app/history/layout.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-// export default function PredictLayout({ children }) {
-//     return (
-//     <div>
-//         {children}
-//     </div>
-//     );
-// }
+export default function PredictLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <div>{children}</div>;
+}
